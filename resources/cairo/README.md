bc16a678fef2b32d3570cfe818f51740bfbbd6b0
Julien Eluard <julien.eluard@gmail.com>
2023-09-07 01:30:53 +0200

    Added cairo files based tests

---
+++ resources/cairo/README.md
@@ -0,0 +1,6 @@
+Cairo 0 example files and associated `json` artifacts as produced by https://cairo-lang.org/playground/
+
+To generate `res.json`:
+* hit `Debug` on the playground
+* find the associated `/cairo` HTTP call in your browser dev console
+* copy/paste the HTTP call answer
--- tests/cairo_test.rs
+++ tests/cairo_test.rs
@@ -1,67 +1,14 @@
 #![allow(non_snake_case)]
 
-use ark_std::{end_timer, start_timer};
-use halo2_base::gates::builder::{
-    CircuitBuilderStage, GateThreadBuilder, MultiPhaseThreadBreakPoints, RangeCircuitBuilder,
-};
+use common::vm_circuit;
+use halo2_base::gates::builder::CircuitBuilderStage;
 use halo2_base::halo2_proofs::{dev::MockProver, halo2curves::bn256::Fr};
 use halo2_base::utils::ScalarField;
-use halo2_base::Context;
-use halo2_cairo::cairo::CairoChip;
-use halo2_cairo::cairo::CairoVM;
+
+mod common;
+
 use halo2_cairo::generate_test_data_for_program_with_builtin;
-use halo2_cairo::MAX_NUM_CYCLES;
 const DEGREE: u32 = 11;
-
-fn state_transition_test<F: ScalarField>(
-    ctx: &mut Context<F>,
-    pc: F,
-    ap: F,
-    fp: F,
-    memory: Vec<F>,
-    expected_next_pc: F,
-    expected_next_ap: F,
-    expected_next_fp: F,
-) {
-    let cairo_state = CairoChip::<F, 10>::new();
-
-    let [pc, ap, fp] = [pc, ap, fp].map(|x| ctx.load_witness(x));
-    let memory = ctx.assign_witnesses(memory);
-
-    let (next_pc, next_ap, next_fp) = cairo_state.state_transition(ctx, &memory, pc, ap, fp);
-    assert_eq!(*next_pc.value(), expected_next_pc);
-    assert_eq!(*next_ap.value(), expected_next_ap);
-    assert_eq!(*next_fp.value(), expected_next_fp);
-}
-
-fn vm_test<F: ScalarField>(
-    ctx: &mut Context<F>,
-    pc: F,
-    ap: F,
-    fp: F,
-    num_cycles: F,
-    memory: Vec<F>,
-    expected_final_pc: F,
-    expected_final_ap: F,
-    expected_final_fp: F,
-) {
-    let cairo_chip = CairoChip::<F, MAX_NUM_CYCLES>::new();
-
-    let [pc, ap, fp] = [pc, ap, fp].map(|x| ctx.load_witness(x));
-    let memory = ctx.assign_witnesses(memory);
-    let num_cycles = ctx.load_witness(num_cycles);
-
-    let (final_pc, final_ap, final_fp, is_valid_transition) =
-        cairo_chip.vm(ctx, &memory, pc, ap, fp, num_cycles);
-
-    // check register matches
-    assert_eq!(*final_pc.value(), expected_final_pc);
-    assert_eq!(*final_ap.value(), expected_final_ap);
-    assert_eq!(*final_fp.value(), expected_final_fp);
-
-    // check execution trace is valid
-    assert_eq!(*is_valid_transition.value(), F::one());
-}
 
 fn generate_test_data_for_fibonacci_program<F: ScalarField>() -> (Vec<F>, Vec<[F; 3]>) {
     // [0x48307ffe7fff8000, 0x010780017fff7fff, -1, 1, 2, 3]
@@ -116,125 +63,10 @@ fn generate_test_data_for_fibonacci_program_with_wrong_registers<F: ScalarField>
     (memory, register_traces)
 }
 
-fn state_transition_circuit(
-    pc: Fr,
-    ap: Fr,
-    fp: Fr,
-    memory: Vec<Fr>,
-    expected_next_pc: Fr,
-    expected_next_ap: Fr,
-    expected_next_fp: Fr,
-    stage: CircuitBuilderStage,
-    break_points: Option<MultiPhaseThreadBreakPoints>,
-) -> RangeCircuitBuilder<Fr> {
-    let mut builder = match stage {
-        CircuitBuilderStage::Mock => GateThreadBuilder::mock(),
-        CircuitBuilderStage::Prover => GateThreadBuilder::prover(),
-        CircuitBuilderStage::Keygen => GateThreadBuilder::keygen(),
-    };
-    let start0 = start_timer!(|| format!("Witness generation for circuit in {stage:?} stage"));
-    state_transition_test(
-        builder.main(0),
-        pc,
-        ap,
-        fp,
-        memory,
-        expected_next_pc,
-        expected_next_ap,
-        expected_next_fp,
-    );
-
-    let circuit = match stage {
-        CircuitBuilderStage::Mock => {
-            builder.config(DEGREE as usize, Some(20));
-            RangeCircuitBuilder::mock(builder)
-        }
-        CircuitBuilderStage::Keygen => {
-            builder.config(DEGREE as usize, Some(20));
-            RangeCircuitBuilder::keygen(builder)
-        }
-        CircuitBuilderStage::Prover => RangeCircuitBuilder::prover(builder, break_points.unwrap()),
-    };
-    end_timer!(start0);
-    circuit
-}
-
-fn vm_circuit(
-    pc: Fr,
-    ap: Fr,
-    fp: Fr,
-    num_cycles: Fr,
-    memory: Vec<Fr>,
-    expected_final_pc: Fr,
-    expected_final_ap: Fr,
-    expected_final_fp: Fr,
-    stage: CircuitBuilderStage,
-    break_points: Option<MultiPhaseThreadBreakPoints>,
-) -> RangeCircuitBuilder<Fr> {
-    let mut builder = match stage {
-        CircuitBuilderStage::Mock => GateThreadBuilder::mock(),
-        CircuitBuilderStage::Prover => GateThreadBuilder::prover(),
-        CircuitBuilderStage::Keygen => GateThreadBuilder::keygen(),
-    };
-    let start0 = start_timer!(|| format!("Witness generation for circuit in {stage:?} stage"));
-    vm_test(
-        builder.main(0),
-        pc,
-        ap,
-        fp,
-        num_cycles,
-        memory,
-        expected_final_pc,
-        expected_final_ap,
-        expected_final_fp,
-    );
-
-    let circuit = match stage {
-        CircuitBuilderStage::Mock => {
-            builder.config(DEGREE as usize, Some(20));
-            RangeCircuitBuilder::mock(builder)
-        }
-        CircuitBuilderStage::Keygen => {
-            builder.config(DEGREE as usize, Some(20));
-            RangeCircuitBuilder::keygen(builder)
-        }
-        CircuitBuilderStage::Prover => RangeCircuitBuilder::prover(builder, break_points.unwrap()),
-    };
-    end_timer!(start0);
-    circuit
-}
-
 #[test]
 fn test_cairo_fibonacci_program() {
     let (memory, register_traces) = generate_test_data_for_fibonacci_program::<Fr>();
-    for i in 0..register_traces.len() - 1 {
-        let (pc, ap, fp) = (
-            register_traces[i][0],
-            register_traces[i][1],
-            register_traces[i][2],
-        );
-        let (next_pc, next_ap, next_fp) = (
-            register_traces[i + 1][0],
-            register_traces[i + 1][1],
-            register_traces[i + 1][2],
-        );
-        let circuit = state_transition_circuit(
-            pc,
-            ap,
-            fp,
-            memory.clone(),
-            next_pc,
-            next_ap,
-            next_fp,
-            CircuitBuilderStage::Mock,
-            None,
-        );
-
-        // equality contraints hold
-        MockProver::run(DEGREE, &circuit, vec![])
-            .unwrap()
-            .assert_satisfied();
-    }
+    common::run_mock_prover(memory, register_traces);
 }
 
 #[test]
@@ -274,32 +106,5 @@ fn test_cairo_vm_program_with_builtin() {
 fn test_cairo_fibonacci_program_with_wrong_registers() {
     let (memory, register_traces) =
         generate_test_data_for_fibonacci_program_with_wrong_registers::<Fr>();
-    for i in 0..register_traces.len() - 1 {
-        let (pc, ap, fp) = (
-            register_traces[i][0],
-            register_traces[i][1],
-            register_traces[i][2],
-        );
-        let (next_pc, next_ap, next_fp) = (
-            register_traces[i + 1][0],
-            register_traces[i + 1][1],
-            register_traces[i + 1][2],
-        );
-        let circuit = state_transition_circuit(
-            pc,
-            ap,
-            fp,
-            memory.clone(),
-            next_pc,
-            next_ap,
-            next_fp,
-            CircuitBuilderStage::Mock,
-            None,
-        );
-
-        // equality contraints hold
-        MockProver::run(DEGREE, &circuit, vec![])
-            .unwrap()
-            .assert_satisfied();
-    }
+    common::run_mock_prover(memory, register_traces);
 }
